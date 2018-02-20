36eb5ac600da14abff822a54e4063698a99864af
Arnav Gupta <arnav@codingblocks.com>
2018-02-20 20:37:57 +0530

    more coverage for .js files
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-promise-utils'
+  name: 'ember-promise-utils',
+  options: {
+    'babel': {
+      sourceMaps: 'both'
+    },
+    'ember-cli-babel': {
+      sourceMaps: 'both'
+    }
+  }
 };
