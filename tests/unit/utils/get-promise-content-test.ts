b7dbbb93475a9501f0d0db2b2dbfa402f8010360
Arnav Gupta <arnav@codingblocks.com>
2018-02-20 05:50:01 +0530

    smart resolve, with tests
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ tests/unit/utils/get-promise-content-test.ts
@@ -0,0 +1,73 @@
+import DS from 'ember-data';
+import { defer } from 'rsvp'
+import isFulfilled from 'ember-promise-utils/utils/is-fulfilled';
+import isPromise from 'ember-promise-utils/utils/is-promise';
+import getPromiseContent from 'ember-promise-utils/utils/get-promise-content';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | get-promise-content', function() {
+
+  test('Ember Promise Proxymixin gets fulfilled content', function(assert) {
+    assert.expect(4);
+
+    let deferred = defer();
+    let promiseObject = DS.PromiseObject.create({ promise: deferred.promise });
+
+    assert.ok(isPromise(promiseObject));
+
+    assert.equal(isFulfilled(promiseObject), false);
+
+    deferred.resolve('done');
+
+    let done = assert.async();
+
+    deferred.promise.then(() => {
+      assert.equal(isFulfilled(promiseObject), true);
+      assert.equal(getPromiseContent(promiseObject), 'done');
+      done();
+    });
+  });
+
+  test('RSVP Promise mixin gets fulfilled content', function(assert) {
+    assert.expect(4);
+
+    let deferred = defer();
+
+    assert.ok(isPromise(deferred.promise));
+    assert.equal(isFulfilled(deferred.promise), false);
+
+    deferred.resolve('done');
+    let done = assert.async();
+
+    deferred.promise.then(() => {
+      assert.equal(isFulfilled(deferred.promise), true);
+      assert.equal(getPromiseContent(deferred.promise), 'done');
+      done();
+    });
+  });
+
+  test('Duck typed promise cant get fulfilled content', function(assert) {
+    assert.expect(6);
+
+    let promise = {
+      then(result) {
+        this.done = true;
+        this.result = result;
+      },
+      catch() {},
+      done: false,
+      result: null
+    };
+
+    assert.ok(isPromise(promise));
+
+    assert.equal(isFulfilled(promise), false);
+
+    promise.then('done');
+
+    assert.equal(isFulfilled(promise), false);
+    assert.equal(promise.done, true);
+    assert.equal(getPromiseContent(promise), null);
+    assert.equal(promise.result, 'done');
+  });
+});
---
+++ tests/unit/utils/smart-resolve-test.js
@@ -0,0 +1,99 @@
+import { defer } from 'rsvp';
+import DS from 'ember-data'
+import smartResolve from 'ember-promise-utils/utils/smart-resolve';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | smart-resolve', function() {
+
+  test('Smart Resolve returns non promise value', function(assert) {
+    assert.expect(1);
+
+    let value = 4;
+
+    let returnValue = smartResolve(value);
+
+    assert.equal(returnValue, value);
+  });
+
+  test('Smart Resolve returns unresolved Ember Promise Proxy mixin', function(assert) {
+    assert.expect(1);
+
+    let deferred = defer();
+    let promiseObject = DS.PromiseObject.create({ promise: deferred.promise });
+
+    let returnValue = smartResolve(promiseObject);
+
+    assert.equal(returnValue, promiseObject);
+  });
+
+  test('Smart Resolve returns unresolved RSVP Promise', function(assert) {
+    assert.expect(1);
+
+    let deferred = defer();
+
+    let returnValue = smartResolve(deferred.promise);
+
+    assert.equal(returnValue, deferred.promise);
+  });
+
+  test('Smart Resolve returns Duck typed promise', function(assert) {
+    assert.expect(1);
+
+    let promise = { then() {}, catch() {} };
+
+    let returnValue = smartResolve(promise);
+
+    assert.equal(returnValue, promise);
+  });
+
+  test('Smart Resolve returns resolved Ember Promise Proxy mixin value', function(assert) {
+    assert.expect(1);
+
+    let deferred = defer();
+    let promiseObject = DS.PromiseObject.create({ promise: deferred.promise });
+
+    deferred.resolve('done');
+
+    let done = assert.async();
+
+    deferred.promise.then(() => {
+      let returnValue = smartResolve(promiseObject);
+      assert.equal(returnValue, 'done');
+      done();
+    });
+  });
+
+  test('Smart Resolve returns resolved RSVP Promise value', function(assert) {
+    assert.expect(1);
+
+    let deferred = defer();
+
+    deferred.resolve('done');
+
+    let done = assert.async();
+
+    deferred.promise.then(() => {
+      let returnValue = smartResolve(deferred.promise);
+      assert.equal(returnValue, 'done');
+      done();
+    });
+  });
+
+  test('Smart Resolve does not return resolved Duck typed promise value', function(assert) {
+    assert.expect(2);
+
+    let promise = {
+      then() { this.done = true; },
+      catch() {},
+      done: false
+    };
+
+    promise.then();
+
+    let returnValue = smartResolve(promise);
+    assert.notEqual(returnValue, true);
+    assert.equal(promise.done, true);
+  });
+
+
+});
